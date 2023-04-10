bd5fab0837468364887453882796edf0cc3eaff7
medino98 <47697754+medino98@users.noreply.github.com>
2023-04-10 07:59:57 +0200

    Just set up CMake and IntelliSense to work properly with msvc

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Programming_workspace/C_C++_workspace/BeerPong-robot-software
+# Build directory: C:/Programming_workspace/C_C++_workspace/BeerPong-robot-software/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
