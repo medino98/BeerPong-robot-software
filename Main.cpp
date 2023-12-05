6772be3a2d39bce45948a542dba048a034b7e616
medino98 <47697754+medino98@users.noreply.github.com>
2023-12-05 06:52:12 +0100

    update

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,18 @@
 #include <conio.h>
-
-#include "opencv2/opencv.hpp"
-
 #include <iostream>
+#include <stdio.h>
+#include <string>
+#include <fstream>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 using namespace std;
 using namespace cv;
+
+#define WRITE_PARAMS_TO_FILE
 
 // Buffers
 Mat frame;
@@ -27,6 +34,12 @@ vector<Rect> selections;
 //Various variables
 int thresh = 40;
 bool start_process = false;
+
+// Defining the unit dimensions of checkerboard
+int checkerboard[2]{5,8}; 
+
+// Size of one square of the calibration chessboard
+int sizeOfSquare = 30;
 
 //General mouse handling for selection of ROI
 static bool showSelection()
@@ -65,10 +78,108 @@ static void onMouse(int event, int x, int y, int, void*)
 	}
 }
 
-int main(int, char**)
+
+void CalibrateCamera()
 {
+  // Creating vector to store vectors of 3D points for each checkerboard image
+  std::vector<std::vector<cv::Point3f>> objpoints;
+ 
+  // Creating vector to store vectors of 2D points for each checkerboard image
+  std::vector<std::vector<cv::Point2f>> imgpoints;
+ 
+  // Defining the world coordinates for 3D points
+  std::vector<cv::Point3f> objp;
+  for(int i{0}; i<checkerboard[1]; i++)
+  {
+    for(int j{0}; j<checkerboard[0]; j++)
+	{
+      objp.push_back(cv::Point3f(j*sizeOfSquare,i*sizeOfSquare,0));
+	}
+
+  }
+ 
+ 
+  // Extracting path of individual image stored in a given directory
+  std::vector<cv::String> images;
+  // Path of the folder containing checkerboard images
+  std::string path = "C:\\Programming_workspace\\C_C++_workspace\\BeerPong-robot-software\\images";
+ 
+  cv::glob(path, images);
+ 
+  cv::Mat frame, gray, undist_frame;
+  // vector to store the pixel coordinates of detected checker board corners 
+  std::vector<cv::Point2f> corner_pts;
+  bool success;
+ 
+  // Looping over all the images in the directory
+  for(int i{0}; i<images.size(); i++)
+  {
+    frame = cv::imread(images[i]);
+    cv::cvtColor(frame,gray,cv::COLOR_BGR2GRAY);
+ 
+    // Finding checker board corners
+    // If desired number of corners are found in the image then success = true  
+    success = cv::findChessboardCorners(gray, cv::Size(checkerboard[0], checkerboard[1]), corner_pts, CALIB_CB_ADAPTIVE_THRESH | CALIB_CB_FAST_CHECK | CALIB_CB_NORMALIZE_IMAGE);
+     
+    /* 
+     * If desired number of corner are detected,
+     * we refine the pixel coordinates and display 
+     * them on the images of checker board
+    */
+    if(success)
+    {
+      cv::TermCriteria criteria(cv::TermCriteria::EPS | cv::TermCriteria::MAX_ITER , 30, 0.001);
+       
+      // refining pixel coordinates for given 2d points.
+      cv::cornerSubPix(gray,corner_pts,cv::Size(11,11), cv::Size(-1,-1),criteria);
+       
+      // Displaying the detected corner points on the checker board
+      cv::drawChessboardCorners(frame, cv::Size(checkerboard[0], checkerboard[1]), corner_pts, success);
+       
+      objpoints.push_back(objp);
+      imgpoints.push_back(corner_pts);
+    }
+	
+    cv::imshow("Image",frame);
+    cv::waitKey(0);
+  }
+ 
+  cv::destroyAllWindows();
+ 
+  cv::Mat cameraMatrix,distCoeffs,R,T;
+ 
+  /*
+   * Performing camera calibration by 
+   * passing the value of known 3D points (objpoints)
+   * and corresponding pixel coordinates of the 
+   * detected corners (imgpoints)
+  */
+  cv::calibrateCamera(objpoints, imgpoints, cv::Size(gray.rows,gray.cols), cameraMatrix, distCoeffs, R, T);
+
+#ifdef WRITE_PARAMS_TO_FILE
+	cv::FileStorage fs("camera_calibration.xml", cv::FileStorage::WRITE);
+    fs << "cameraMatrix" << cameraMatrix;
+    fs << "distortionCoeffs" << distCoeffs;
+	fs << "Rotation vector : " << R;
+	fs << "Translation vector : " << T;
+    fs.release();
+#else 
+	cout << "cameraMatrix : " << cameraMatrix << endl;
+	cout << "distCoeffs : " << distCoeffs << endl;
+	cout << "Rotation vector : " << R << endl;
+	cout << "Translation vector : " << T << endl;
+#endif
+
+	
+
+}
+
+int findEllpise()
+{
+	// Init video object
+	VideoCapture cap;
 	// Open the default camera
-	VideoCapture cap(0);
+	cap.open(0);
 	// Check if we succeeded
 	if (!cap.isOpened())
 	{
@@ -86,7 +197,13 @@ int main(int, char**)
 	while (true)
 	{
 		// Get a new frame from camera
-		cap >> frame;
+		cap.read(frame);
+
+		// check if we succeeded
+        if (frame.empty()) {
+            cerr << "ERROR! blank frame grabbed\n";
+            break;
+        }
 
 		if (canvas.empty()) canvas = Mat::zeros(frame.size(), frame.type());
 
@@ -161,7 +278,14 @@ int main(int, char**)
 		}
 
 		if (key == 27) break;
+		return 0;
 	}
+}
+
+
+int main(int, char**)
+{
+	CalibrateCamera();
 	// The camera will be deinitialized automatically in VideoCapture destructor
 	return 0;
 }
